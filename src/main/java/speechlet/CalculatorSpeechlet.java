98eb3d2ea104b40427041098d4b12e9f622820d4
jMerta <mer.kuba@gmail.com>
2017-02-22 00:18:36 +0100

    Refactor

--- src/main/java/speechlet/CalculatorSpeechlet.java
+++ src/main/java/speechlet/CalculatorSpeechlet.java
@@ -3,17 +3,13 @@ package speechlet;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.*;
-import com.amazon.speech.ui.Card;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
-import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 import domain.operations.linear.LinearEquations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import speechlet.handler.IOperationsHandler;
 import speechlet.handler.OperationsHandler;
 
-import javax.xml.ws.Response;
 import java.util.Map;
 
 /**
@@ -22,7 +18,7 @@ import java.util.Map;
 public class CalculatorSpeechlet implements Speechlet {
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorSpeechlet.class);
-    private IOperationsHandler operationsHandler = new OperationsHandler();
+    private OperationsHandler operationsHandler = new OperationsHandler();
 
     @Override
     public void onSessionStarted(SessionStartedRequest sessionStartedRequest, Session session) throws SpeechletException {
@@ -64,68 +60,29 @@ public class CalculatorSpeechlet implements Speechlet {
             z = Integer.valueOf(map.get("zValue").getValue());
         }
 
-        switch (intentName) {
 
-            case "AMAZON.StopIntent": {
-                speech.setText("Goodbye");
-                break;
-            }
-            case "SimpleOperation": {
-                speech.setText(operationsHandler.simpleOperations().operator(x, y, map.get("operation").getValue()));
-                break;
-            }
-            case "LinearEquation": {
-                LinearEquations linearEquations = new LinearEquations();
-                linearEquations.setA(x);
-                linearEquations.setB(y);
-                speech.setText(String.valueOf(linearEquations.calculateX()));
-                break;
-            }
-            case "QuadraticEquation": {
-                speech.setText(operationsHandler.quadraticOperations().operator(x, y, z, map.get("kind").getValue()));
-                break;
-            }
-
-            case "CirceOperation": {
-                speech.setText(operationsHandler.circleOperations().operator(x, map.get("CircleOp").getValue()));
-                break;
-            }
-            case "CubeOperation": {
-                speech.setText(operationsHandler.cubeOperations().operator(x, map.get("CubeOp").getValue()));
-                break;
-            }
-            case "TriangleOperation": {
-                speech.setText(operationsHandler.triangleOperations().operator(x, y, z, map.get("TriangleOp").getValue()));
-                break;
-            }
-            case "CuboidOperation": {
-                speech.setText(operationsHandler.cuboidOperations().operator(x, y, z, map.get("CuboidOp").getValue()));
-                break;
-            }
-            case "PyramidOperation": {
-                speech.setText(operationsHandler.pyramidOperations().operator(x, y, z, map.get("PyramidOp").getValue()));
-                break;
-            }
-            case "RectangleOperation": {
-                speech.setText(operationsHandler.rectangleOperations().operator(x, y, map.get("RectangleOp").getValue()));
-                break;
-            }
-            case "SphereOperation": {
-                speech.setText(operationsHandler.sphereOperations().operator(x, map.get("SphereOp").getValue()));
-                break;
-            }
-
-            case "SquareOperation": {
-                speech.setText(operationsHandler.sphereOperations().operator(x, map.get("SphereOp").getValue()));
-                break;
-            }
-            default:
-                throw new SpeechletException("Invalid intent");
+        if (map.containsKey("xValue") && !map.containsKey("yValue") && !map.containsKey("zValue") && !intentName.equals("AMAZON.StopIntent")){
+            speech.setText(operationsHandler.singleVariableHandler(intentName,x,map.get("operation").getValue()));
         }
+        else if (map.containsKey("xValue") && map.containsKey("yValue") && !map.containsKey("zValue") && !intentName.equals("AMAZON.StopIntent")){
+            speech.setText(operationsHandler.TwoVariableHandler(intentName,x,y,map.get("operation").getValue()));
+        }
+        else if (map.containsKey("xValue") && map.containsKey("yValue") && map.containsKey("zValue") && !intentName.equals("AMAZON.StopIntent")) {
+            speech.setText(operationsHandler.ThreeVariableHandler(intentName,x,y,z,map.get("operation").getValue()));
+        }
+        else if (intentName.equals("AMAZON.StopIntent")){
+            speech.setText("Goodbye");
+        }
+        else
+        {
+            throw new SpeechletException("Invalid intent");
+        }
+
+
+
 
         card.setTitle(intentName);
         card.setContent(speech.getText());
-
         response.setOutputSpeech(speech);
         response.setCard(card);
         response.setShouldEndSession(false);
